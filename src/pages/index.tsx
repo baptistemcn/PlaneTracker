4072b3f7eb43c97c29a542e62ebecbb79d0ff6f9
Baptiste Marcon <baptistemcn@icloud.com>
2023-12-30 19:39:43 +0100

    feat(home): create message and title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,21 @@ export default function PlaneTracker() {
   return (
     <>
       <NextSeo title={"PlaneTracker - Work in Progress"} />
-      <div className="flex min-h-screen flex-row  items-center justify-center">
-        <h1 className="text-3xl font-bold underline" data-testid="title">
-          PlaneTracker
-        </h1>
+
+      <div className="min-h-screen">
+        <div className="mx-auto flex min-h-screen w-full max-w-[1120px]  flex-col items-center justify-center px-5 py-16 md:px-10 md:py-24 lg:py-32">
+          <div className="lg:max-w-[80%]">
+            <h1
+              className="mb-5 text-4xl font-semibold md:text-8xl lg:mb-8"
+              data-testid="title"
+            >
+              PlaneTracker
+            </h1>
+            <p className="mb-5 max-w-[528px] text-center text-xl text-[#636262] lg:mb-8">
+              Coming soon...
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
